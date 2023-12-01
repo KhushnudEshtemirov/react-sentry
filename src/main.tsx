9c04addb710fdfa87cbd37af36626a4d2da1bc3d
Khushnud Eshtemirov <khushnudeshtemirov@gmail.com>
2023-12-01 11:09:27 +0500

    Updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+Sentry.init({
+  dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
+  integrations: [
+    new Sentry.BrowserTracing({
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+
+  tracesSampleRate: 1.0,
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
